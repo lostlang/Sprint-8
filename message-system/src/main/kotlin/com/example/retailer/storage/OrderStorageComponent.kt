c47698312ea9485111bffbb8b0d06b8eb9eae2e6
lostlang <53492781+lostlang@users.noreply.github.com>
2021-11-22 01:38:06 +0300

    OrderStorage complete

---
+++ message-system/src/main/kotlin/com/example/retailer/storage/OrderStorageComponent.kt
@@ -0,0 +1,41 @@
+package com.example.retailer.storage
+
+import com.example.retailer.api.distributor.Order
+import com.example.retailer.api.distributor.OrderInfo
+import com.example.retailer.api.distributor.OrderStatus
+import com.example.retailer.repository.OrderInfoRepository
+import com.example.retailer.repository.OrderRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Component
+
+@Component
+class OrderStorageComponent : OrderStorage {
+
+    @Autowired
+    lateinit var orderRepository: OrderRepository
+
+    @Autowired
+    lateinit var orderInfoRepository: OrderInfoRepository
+
+    override fun createOrder(draftOrder: Order): PlaceOrderData {
+        val order = orderRepository.save(draftOrder)
+        val orderInfo = orderInfoRepository.save(OrderInfo(order.id!!, OrderStatus.SENT, "lostlang"))
+
+        return PlaceOrderData(order, orderInfo)
+    }
+
+    override fun updateOrder(order: OrderInfo): Boolean {
+        if (orderRepository.existsById(order.orderId)) {
+            orderInfoRepository.save(order)
+
+            return true
+        }
+
+        return false
+    }
+
+    override fun getOrderInfo(id: String): OrderInfo? {
+        return orderInfoRepository.findByIdOrNull(id)
+    }
+}
